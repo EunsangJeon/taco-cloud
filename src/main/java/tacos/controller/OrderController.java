df36f1facf096e8cd6a73300022ad7689d1562c7
EunsangJeon <eunsang.jeon.127@gmail.com>
2021-04-06 21:46:53 +0900

    order added

--- src/main/java/tacos/controller/OrderController.java
+++ src/main/java/tacos/controller/OrderController.java
@@ -4,7 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import tacos.model.OrderDto;
 
 @Slf4j
 @Controller
@@ -13,9 +15,13 @@ public class OrderController {
 
     @GetMapping("/current")
     public String orderForm(Model model) {
-        // TODO OrderDto
-        // TODO order form ui
-        // model.addAttribute("order", new Order());
+        model.addAttribute("order", new OrderDto());
         return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(OrderDto order) {
+        log.info("Order submitted " + order);
+        return "redirect:/";
     }
 }
---
+++ src/main/java/tacos/model/OrderDto.java
@@ -0,0 +1,16 @@
+package tacos.model;
+
+import lombok.Data;
+
+@Data
+public class OrderDto {
+
+    private String deliveryName;
+    private String deliveryStreet;
+    private String deliveryCity;
+    private String deliveryState;
+    private String deliveryZip;
+    private String ccNumber;
+    private String ccExpiration;
+    private String ccCVV;
+}
